3a6eae7676c329fe7c007967b2398c49e6b538a7
neonguyen-dev <neo20030125@gmail.com>
2023-05-04 08:56:58 +0200

    CashPaymentTest is initiated and done

---
+++ src/test/kth/iv1350/seminar3/model/CashPaymentTest.java
@@ -0,0 +1,46 @@
+package test.kth.iv1350.seminar3.model;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import se.kth.iv1350.seminar3.ItemDTO;
+import se.kth.iv1350.seminar3.VatRate;
+import se.kth.iv1350.seminar3.model.CashPayment;
+import se.kth.iv1350.seminar3.model.Sale;
+
+public class CashPaymentTest {
+    @Test
+    public void testCalculatePayment() {
+        ItemDTO item = new ItemDTO(0, "Köttbullar", 25.0f, VatRate.twentyFive, 100);
+        Sale sale = new Sale();
+        sale.updateSaleInfo(item, 2);
+
+        int paidAmount = 100;
+
+        CashPayment payment = new CashPayment(paidAmount);
+        payment.calculatePayment(sale.getAmount());
+
+        String expectedResult = Integer.toString(Math.round(sale.getAmount().getRunningTotal() + 0.5f));
+        String result = Integer.toString(payment.getTotal());
+
+        assertTrue(result.contains(expectedResult));
+    }
+
+    @Test
+    public void testGetChange() {
+        ItemDTO item = new ItemDTO(0, "Köttbullar", 25.0f, VatRate.twentyFive, 100);
+        Sale sale = new Sale();
+        sale.updateSaleInfo(item, 2);
+
+        int paidAmount = 100;
+
+        CashPayment payment = new CashPayment(paidAmount);
+        payment.calculatePayment(sale.getAmount());
+
+        String expectedResult = Integer.toString(paidAmount - payment.getTotal());
+        String result = Float.toString(payment.getChange());
+
+        assertTrue(result.contains(expectedResult));
+    }
+}
